0181dbfb5153676be30906b3aa56e5e2e6ecd69a
Nicolas Molina Monroy <hi@nicobytes.com>
2021-04-18 20:39:56 -0400

    :bug: Fix assert

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -1,22 +1,22 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'npm run start',
+      startServerCommand: "npm run start",
       numberOfRuns: 3,
       url: [
-        'http://localhost:8080',
-        'http://localhost:8080/about',
-        'http://localhost:8080/service',
+        "http://localhost:8080",
+        "http://localhost:8080/about",
+        "http://localhost:8080/service",
       ],
     },
-    asserts: {
+    assert: {
       assertions: {
-        'categories:performance': ['error', {minScore: .9}],
-        'categories:accessibility': ['error', {minScore: .9}],
-        'categories:best-practices': ['error', {minScore: .9}],
-        'categories:seo': ['error', {minScore: .9}],
-				'categories.pwa': 'off',
-      }
+        "categories:performance": ["error", { minScore: .8 }],
+        "categories:accessibility": ["error", { minScore: .8 }],
+        "categories:best-practices": ["error", { minScore: .8 }],
+        "categories:seo": ["error", { minScore: .8 }],
+        "categories.pwa": "off",
+      },
     },
     upload: {
       target: "temporary-public-storage",
--- lighthouserc.js
+++ lighthouserc.js
@@ -1,14 +1,14 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'npm run start',
+      startServerCommand: "npm run start",
       numberOfRuns: 1,
       url: [
-        'http://localhost:8080',
-        'http://localhost:8080/about',
-        'http://localhost:8080/service',
+        "http://localhost:8080",
+        "http://localhost:8080/about",
+        "http://localhost:8080/service",
       ],
-      chromePath: '/usr/bin/google-chrome',
+      chromePath: "/usr/bin/google-chrome",
     },
     upload: {
       target: "temporary-public-storage",
